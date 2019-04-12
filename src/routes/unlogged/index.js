8c7da54256f80fd67c17f8bfcdb14b9316570dfe
Luis Ventura <luisigsystem@gmail.com>
2019-04-12 17:16:01 -0500

    added views total login and signup

--- src/routes/unlogged/index.js
+++ src/routes/unlogged/index.js
@@ -4,6 +4,12 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         return res.render('index')
     })
+    app.get('/login', (req, res) => {
+        return res.render('costumer/login')
+    })
+    app.get('/signup', (req, res) => {
+        return res.render('costumer/signup')
+    })
     app.get('/partner/login', (req, res) => {
         return res.render('partner/login')
     })
