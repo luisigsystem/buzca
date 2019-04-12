06a8107c56e044c796f47093e15ce5a8e805e1a0
Luis Ventura <luisigsystem@gmail.com>
2019-04-12 17:04:25 -0500

    hide morgan

--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@ const { port } = require('./app/config/config')
 const express = require('express')
 const app = express()
 const path = require('path')
-const morgan = require('morgan') ////
+// const morgan = require('morgan') ////
 
 // Settings
 app.set('view engine', 'pug') 
 app.set('views', path.join(__dirname, 'views'))
 
 // Middlewares
-app.use(morgan('dev')) ////
+// app.use(morgan('dev')) ////
 app.use(express.static( path.join( __dirname, "public") ))
 
 // Routes
