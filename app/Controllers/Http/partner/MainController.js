4883d4d56301f3f1f7c73955aac97291036ec426
Luis Ventura <luisigsystem@gmail.com>
2019-05-09 14:58:58 -0500

    fix get data profile

--- app/Controllers/Http/partner/MainController.js
+++ app/Controllers/Http/partner/MainController.js
@@ -8,18 +8,25 @@ class MainController {
         return view.render('partner/main')
     }
 
-    async loadProfile({request, response, view}) {
-        response.implicitEnd = false
-        
-        axios.get('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/buzcaapp-dnwhd/service/main_application/incoming_webhook/getProfile', {
+    async loadProfile({request, response, view, session}) {
+
+        let email = 'f@f.com'
+
+        axios.post('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/buzcaapp-dnwhd/service/main_application/incoming_webhook/getProfile', {
             email: email
         })
         .then(function(res) {
+            console.log(res.data.user)
+            session.put('user', 'holi')
             return view.render('partner/profile')
         })
         .catch(function(error) {
             return response.send('No Excelente')
         })
+    }
+
+    async loadView({request, view, response, auth}) {
+
     }
 }
 
