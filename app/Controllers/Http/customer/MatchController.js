4afa3cea3401d695c86e304753fc1a5e0a96a9ac
Luis Ventura <luisigsystem@gmail.com>
2019-07-04 17:39:40 -0500

    get distance between user -partner

--- app/Controllers/Http/customer/MatchController.js
+++ app/Controllers/Http/customer/MatchController.js
@@ -7,17 +7,31 @@ class MatchController {
     async index({request, response, view, auth}) {
         try {
             await auth.check()
-            let username = request.params.partner
+            let usernamePartner = request.params.partner
 
-            let res = await axios.post('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/buzcaapp-dnwhd/service/main_application/incoming_webhook/getUser', {
-                username: username
+            let partner = await axios.post('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/buzcaapp-dnwhd/service/main_application/incoming_webhook/getUser', {
+                username: usernamePartner
             })
-            if(res.error) return response.send('Hubo un error')
-            
-            console.error(res.data.user)
-            return view.render('costumer/contact', { partner: res.data.user })
+            if(partner.error) return response.send('Hubo un error')
 
+            let customer = await axios.post('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/buzcaapp-dnwhd/service/main_application/incoming_webhook/getUser', {
+                username: auth.user.username
+            })
+            if(partner.error) return response.send('Hubo un error')
+
+            let dist = await axios.post('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/buzcaapp-dnwhd/service/main_application/incoming_webhook/getDistance', {
+                latitude_a: customer.data.user.lastPosition[0].$numberDouble,
+                longitude_a: customer.data.user.lastPosition[1].$numberDouble,
+                latitude_b: partner.data.user.lastPosition[0].$numberDouble,
+                longitude_b: partner.data.user.lastPosition[1].$numberDouble,
+            })
+            if(dist.error) return response.send('Hubo un error '+dist.error)
+            
+            console.error(dist.data)
+            return view.render('costumer/contact', { partner: partner.data.user, dist: dist.data })
+            
         } catch (error) {
+            console.log(error)
             return response.redirect('/login')
         }
     }
